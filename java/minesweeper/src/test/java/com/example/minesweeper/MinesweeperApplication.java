d8524aa50da17814cacd88d06ea66f1875249bd3
kikenaanhelyna-afk <kikenaanhelyna@gmail.com>
2026-05-12 14:05:42 -0700

    Java modifications

--- java/minesweeper/src/test/java/com/example/minesweeper/MinesweeperApplication.java
+++ java/minesweeper/src/test/java/com/example/minesweeper/MinesweeperApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MinesweeperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MinesweeperApplication.class, args);
-	mine_swipper.start();
+    public void main(String[] args) {
+        SpringApplication.run(MinesweeperApplication.class, args);
+    
+		mine_swipper.start();
 	}
-
 }
